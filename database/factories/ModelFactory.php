d5b84a98a6205109952f1e5e574f9897adcfa33e
Veo Chen <veoc.dev@gmail.com>
2016-07-14 05:40:19 +0000

    force unique value pairs in lectures seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -33,10 +33,21 @@ $factory->define(App\Models\User\Teacher::class, function (Faker\Generator $fake
 });
 
 $factory->define(App\Models\Course\Lecture::class, function (Faker\Generator $faker) {
+    do {
+        $randomNumber = $faker->unique()->randomNumber(3, true);
+        $teacher_id = (int) floor($randomNumber / 100);
+        $day_number = (int) floor(($randomNumber % 100) / 10);
+        $hour_number = $randomNumber % 10 + 9;
+    } while (
+        $day_number < 1
+        OR $day_number >7
+        OR $hour_number > 16
+    );
+
     return [
-        'teacher_id' => mt_rand(1, \App\Models\User\Teacher::count()),
+        'teacher_id' => $teacher_id,
         'student_id' => mt_rand(1, \App\Models\User\Student::count()),
-        'start_at' => \Carbon\Carbon::today()->addDays(mt_rand(1,7))->addHours(mt_rand(9,16))
+        'start_at' => \Carbon\Carbon::today()->addDays($day_number)->addHours($hour_number)
     ];
 });
 
