cb2ca20d68f83e27fe219768f37647331244d860
DM <dm@sudo.team>
2016-07-16 18:13:47 +0800

    Cleaning up mobile wechat/teachers/index

--- resources/views/wechat/layouts/blank.blade.php
+++ resources/views/wechat/layouts/blank.blade.php
@@ -11,16 +11,13 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
     <title>{{ isset($title) ? $title . ' - ' : ''}}{{ appName() }}</title>
 
-    <link href="{{ asset('wechat/lib/jquery.mobile-1.4.5.min.css') }}" rel="stylesheet">
-    <!--
     <link href="{{ asset('wechat/css/weui.css') }}" rel="stylesheet">
     <link href="{{ asset('wechat/css/jquery-weui.css') }}" rel="stylesheet">
     <link href="{{ asset('wechat/css/lewe.css') }}" rel="stylesheet">
-    -->
 </head>
 
 <body>
@@ -31,11 +28,10 @@
 
 <!-- Mainly scripts -->
 <script src="{{ asset('wechat/lib/jquery-2.1.4.min.js') }}"></script>
-<script src="{{ asset('wechat/lib/jquery.mobile-1.4.5.min.js') }}"></script>
-<!--
 <script src="{{ asset('wechat/js/jquery-weui.js') }}"></script>
 <script src="{{ asset('wechat/js/lewe.js') }}"></script>
--->
+
+<!-- Ajax Setup -->
 <script>
     $.ajaxSetup({
         headers: {
@@ -44,6 +40,7 @@
         }
     });
 </script>
+-->
 
 </body>
 
--- resources/views/wechat/teachers/index.blade.php
+++ resources/views/wechat/teachers/index.blade.php
@@ -25,7 +25,7 @@
         <div class="weui_panel_bd">
 
             @foreach($teachers as $teacher)
-                <a href="{{ route('students::teachers.show', $teacher->id) }}" data-transition="slidefade" class="weui_media_box weui_media_appmsg">
+                <a href="{{ route('wechat::teachers.show', $teacher->id) }}" data-transition="slidefade" class="weui_media_box weui_media_appmsg">
                     <div class="weui_media_hd">
                         <img class="weui_media_appmsg_thumb" src="{{ getAvatar($teacher->avatar, 'sm') }}" alt="{{ $teacher->name }}">
                     </div>
