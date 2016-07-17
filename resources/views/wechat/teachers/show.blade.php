10c4db511992cd0d9adf90d1606e3971ff1d2c83
Veo Chen <veoc.dev@gmail.com>
2016-07-17 11:20:42 +0000

    test ogg as first video src

--- resources/views/wechat/teachers/show.blade.php
+++ resources/views/wechat/teachers/show.blade.php
@@ -37,8 +37,8 @@
                         Your browser does not support HTML5 video.
                     </video>
                     <video controls>
-                        <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
                         <source src="http://www.w3schools.com/html/mov_bbb.ogg" type="video/ogg">
+                        <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
                         Your browser does not support HTML5 video.
                     </video>
                 </div>
