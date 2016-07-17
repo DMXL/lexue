fadb0a2c76c33f0efb392d7023633f879b6c1013
Veo Chen <veoc.dev@gmail.com>
2016-07-17 10:40:11 +0000

    test external video

--- resources/views/wechat/teachers/show.blade.php
+++ resources/views/wechat/teachers/show.blade.php
@@ -37,7 +37,7 @@
                         Your browser does not support HTML5 video.
                     </video>
                     <video controls>
-                        <source src="" type="video/mp4">
+                        <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
                         Your browser does not support HTML5 video.
                     </video>
                 </div>
