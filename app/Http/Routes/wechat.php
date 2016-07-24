6b320abd4229038dc642a2f8d3302a2e1e284777
Veo Chen <veoc.dev@gmail.com>
2016-07-24 10:55:01 +0000

    change wechat route prefix to weixin

--- app/Http/Routes/wechat.php
+++ app/Http/Routes/wechat.php
@@ -14,14 +14,14 @@
 
 
 /* General requests - no names needed */
-Route::get('/wechat', 'WechatController@verify');
-Route::post('/wechat', ['as' => 'user_request', 'uses' => 'WechatController@serve']);
+Route::get('weixin', ['as' => 'verify', 'uses' => 'WechatController@verify']);
+Route::post('weixin', ['as' => 'user_request', 'uses' => 'WechatController@serve']);
 
 /* Get menu */
-Route::get('wechat/menu', ['as' => 'menu', 'uses' => 'WechatController@menu']);
+Route::get('weixin/menu', ['as' => 'menu', 'uses' => 'WechatController@menu']);
 
 /*
  * OAuth
  */
-Route::get('wechat/auth', ['as' => 'auth.redirect', 'uses' => 'Auth\WechatAuthController@redirectToProvider']);
-Route::get('wechat/auth/callback', ['as' => 'auth.callback', 'uses' => 'Auth\WechatAuthController@handleProviderCallback']);
+Route::get('weixin/auth', ['as' => 'auth.redirect', 'uses' => 'Auth\WechatAuthController@redirectToProvider']);
+Route::get('weixin/auth/callback', ['as' => 'auth.callback', 'uses' => 'Auth\WechatAuthController@handleProviderCallback']);
