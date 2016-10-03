4e92aa3854329b354943455e8c3bad0bd565b45e
DM <dm@sudo.team>
2016-10-04 00:21:25 +0800

    Resolving conflicts: 	app/Jobs/HandleLecturesPurchased.php

--- app/Listeners/LectureEventListener.php
+++ app/Listeners/LectureEventListener.php
@@ -60,15 +60,30 @@ class LectureEventListener implements ShouldQueue
         $message = [
             'touser'      => $student->wechat_id,
             'template_id' => config('wechat.template.purchase_success'),
-            'url'         => route('m.students::lectures.index'),
-            'topcolor'    => '#00f7f7',
+            'url'         => route('m.students::orders.index'),
+            'topcolor'    => '#000000',
             'data'        => [
-                "first"      => "亲爱的 " . $student->name . "，您已成功购买课程。",
-                "keyword1"   => "<b>" . $teacher->name . "</b> 老师的公开课 <b>" . $lecture->name . "</b>",    // 课程名称
-                "keyword2"   => $order->total . "元",    // 支付金额
-                "keyword3"   => $lecture->date_time,    // 课程时间
-                "remark"     => "请按时进入教室上课，不要迟到哦！"
-            ],
+                "first"      => [
+                    "value" => "亲爱的 " . $student->name . "，您已成功购买课程。\n",
+                    "color" => "#000000"
+                ],
+                "keyword1"   => [
+                    "value" => $teacher->name . " 老师的公开课 " . $lecture->name,    // 课程名称
+                    "color" => "#00beb7"
+                ],
+                "keyword2"   => [
+                    "value" => number_format($order->total, 2) . "元",    // 支付金额
+                    "color" => "#00beb7"
+                ],
+                "keyword3"   => [
+                    "value" => $lecture->date_time,    // 课程时间
+                    "color" => "#00beb7"
+                ],
+                "remark"   => [
+                    "value" => "请按时进入教室上课，不要迟到哦！",
+                    "color" => "#000000"
+                ]
+            ]
         ];
 
         \WechatPusher::push($message);
