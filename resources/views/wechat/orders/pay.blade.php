5a84e3f8417afc739583f48b66cbcf865364c3d3
DM <dm@sudo.team>
2016-10-08 19:04:54 +0800

    Hot test on WeChat JSAPI. #13

--- resources/views/wechat/orders/pay.blade.php
+++ resources/views/wechat/orders/pay.blade.php
@@ -48,8 +48,20 @@
 @section('js')
     <script>
         $('#pay').click(function() {
-            console.log({!! json_encode($attributes) !!});
 
+
+            wx.config({!! $configs !!});
+
+            wx.chooseWXPay({
+                timestamp: "{!! $attributes['timestamp'] !!}",
+                nonceStr: "{!! $attributes['nonceStr'] !!}",
+                package: "{!! $attributes['package'] !!}",
+                signType: "{!! $attributes['signType'] !!}",
+                paySign: "{!! $attributes['paySign'] !!}", // 支付签名
+                success: function (res) {
+                    console.log(res['get_brand_wcpay_request']);
+                }
+            });
         });
     </script>
 @endsection
