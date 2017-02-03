47fc991ba9a0ced0fae2bc83ddbfd86ca79e0d12
DM <dm@sudo.team>
2017-02-03 23:40:10 +0800

    Fixing stapler setup problem. #3

--- config/laravel-stapler/filesystem.php
+++ config/laravel-stapler/filesystem.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'url' => '/:attachment/:id_partition/:style/:filename',
+    'url' => '/uploads/:attachment/:id_partition/:style/:filename',
     /*
     |--------------------------------------------------------------------------
     | Stapler File Path
@@ -27,7 +27,7 @@ return [
     |
     */
 
-    'path' => ':app_root/public/uploads:url',
+    'path' => ':app_root/public:url',
 
     /*
     |--------------------------------------------------------------------------
