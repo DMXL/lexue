043db5afb48bf8f06b1197f39f507fce8d22161a
DM <dm@sudo.team>
2017-02-07 17:01:15 +0800

    Updating WP menu.

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -163,7 +163,7 @@ class WechatController extends Controller
                     [
                         "type" => "view",
                         "name" => "精彩回放",
-                        "url"  => route('m.students::lectures.index')
+                        "url"  => route('m.students::lectures.replays')
                     ],
                 ],
             ]
